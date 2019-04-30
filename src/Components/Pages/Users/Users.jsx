2b7dcd16e4239e2cab5784469f0246d5698b3f10
TauroAlde <amed157@hotmail.com>
2019-04-30 16:04:48 -0500

    [F][NTRA-14]add_button_user_to_add_new_users_with_route_/users/new

--- src/Components/Pages/Users/Users.jsx
+++ src/Components/Pages/Users/Users.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { withStyles }       from '@material-ui/core/styles';
-import LoadingTopBar        from '../../Ui/LoadingTopBar'
-import API         from '../../../axiosConfig'
+import LoadingTopBar        from '../../Ui/LoadingTopBar';
+import API         from '../../../axiosConfig';
 import Table       from '@material-ui/core/Table';
 import TableBody   from '@material-ui/core/TableBody';
 import TableCell   from '@material-ui/core/TableCell';
 import TableHead   from '@material-ui/core/TableHead';
 import TableRow    from '@material-ui/core/TableRow';
 import Paper       from '@material-ui/core/Paper';
-import { styles }  from './styles'
+import { styles }  from './styles';
+import Button      from '@material-ui/core/Button';
+import { Link }    from 'react-router-dom';
+import Grid        from '@material-ui/core/Grid';
+import PersonAddIcon        from '@material-ui/icons/PersonAdd';
 
 class Users extends Component {
   constructor() {
@@ -29,7 +33,14 @@ class Users extends Component {
   render() {
     const { classes } = this.props
     return(
-      <div className={classes.root}>
+      <div className={classes.root} >
+        <div>
+          <Grid   container  direction="row"  justify="flex-end"  alignItems="flex-end" style={{ paddingRight:'23px'}}>
+            <Button component={Link} to="/users/new" variant="contained" color="primary" className={classes.button} >
+              <PersonAddIcon className={classes.rightIcon} />
+            </Button>
+          </Grid>
+        </div> 
         { this.state.loading && <LoadingTopBar/> }
         <div className={classes.tableWrapper}>
           <Paper >
