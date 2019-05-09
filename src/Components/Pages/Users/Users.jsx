32eab94d329f8ba8d67dd61a45b50a50bf6f289d
TauroAlde <amed157@hotmail.com>
2019-05-09 12:58:35 -0500

    [F][NTRA-16] add_form_screen_to_create_new_users and error 422

--- src/Components/Pages/Users/Users.jsx
+++ src/Components/Pages/Users/Users.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import { withStyles }       from '@material-ui/core/styles';
-import API            from '../../../axiosConfig';
-import Table          from '@material-ui/core/Table';
-import TableBody      from '@material-ui/core/TableBody';
-import TableCell      from '@material-ui/core/TableCell';
-import TableHead      from '@material-ui/core/TableHead';
-import TableRow       from '@material-ui/core/TableRow';
-import Paper          from '@material-ui/core/Paper';
-import { styles }     from './styles';
-import Button         from '@material-ui/core/Button';
-import { Link }       from 'react-router-dom';
-import Grid           from '@material-ui/core/Grid';
-import PersonAddIcon  from '@material-ui/icons/PersonAdd';
-import GenericDropdownMenu from '../../Ui/GenericDropdownMenu';
+import LoadingTopBar        from '../../Ui/LoadingTopBar';
+import API                  from '../../../axiosConfig';
+import Table                from '@material-ui/core/Table';
+import TableBody            from '@material-ui/core/TableBody';
+import TableCell            from '@material-ui/core/TableCell';
+import TableHead            from '@material-ui/core/TableHead';
+import TableRow             from '@material-ui/core/TableRow';
+import Paper                from '@material-ui/core/Paper';
+import { styles }           from './styles';
+import Button               from '@material-ui/core/Button';
+import { Link }             from 'react-router-dom';
+import Grid                 from '@material-ui/core/Grid';
+import PersonAddIcon        from '@material-ui/icons/PersonAdd';
+import GenericDropdownMenu  from '../../Ui/GenericDropdownMenu';
 
 class Users extends Component {
   constructor() {
     super()
     this.state = {
       loading: true,
-      users:[] 
+      users:[]
     }
   }
 
@@ -39,12 +40,13 @@ class Users extends Component {
             <PersonAddIcon/>
           </Button>
         </Grid>
+        { this.state.loading && <LoadingTopBar/> }
         <div className={classes.tableWrapper}>
           <Paper >
             <Table className={classes.table}>
               <TableHead>
-                
                 <TableRow>
+                  <TableCell>Dessert (100g serving)</TableCell>
                   <TableCell align="right">Nombre</TableCell>
                   <TableCell align="right">Apellido</TableCell>
                   <TableCell align="right">Correo</TableCell>
@@ -54,6 +56,9 @@ class Users extends Component {
               <TableBody>
                 {this.state.users.map(n => (
                   <TableRow key={n.id}>
+                    <TableCell component="th" scope="row">
+                      {n.id}
+                    </TableCell>
                     <TableCell align="right">{n.first_name}</TableCell>
                     <TableCell align="right">{n.last_name}</TableCell>
                     <TableCell align="right">{n.email}</TableCell>
