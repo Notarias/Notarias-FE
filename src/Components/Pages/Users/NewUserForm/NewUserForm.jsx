060ab8946744ce938590d392f53d4d86b3ff8cef
Alexis Rabago <alexisraca@hotmail.com>
2019-05-15 19:23:10 -0500

    [F] [NTRA-37] Add role dropdown to user creation

--- src/Components/Pages/Users/NewUserForm/NewUserForm.jsx
+++ src/Components/Pages/Users/NewUserForm/NewUserForm.jsx
@@ -1,42 +1,79 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import renderTextField      from './../../../Ui/renderTextField';
+import RoleSelectDropdown   from './RoleSelectDropdown';
 import Button               from '@material-ui/core/Button';
+import API                  from './../../../../axiosConfig';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const requiredFields = {
-  "email": "Correo Electrónico",
-  "password": "Contraseña",
-  "password_confirmation": "Confirmar Contraseña",
-  "first_name": "Nombre",
-  "last_name": "Apellido",
-}
 
 class  NewUser extends Component {
+  constructor() {
+    super()
+    this.state = {
+      requiredFields: {
+        "email": "Correo Electrónico",
+        "password": "Contraseña",
+        "password_confirmation": "Confirmar Contraseña",
+        "first_name": "Nombre",
+        "last_name": "Apellido",
+      },
+      roles: []
+    }
+  }
+
+  componentDidMount() {
+    API.get("/roles").then((response) => {
+      this.setState({ roles: response.data.roles })
+    });
+  }
+
+  requiredFields(errors) {
+    let requiredFields = this.state.requiredFields;
+    return Object.keys(requiredFields).reduce((result, key) => {
+      result.push(
+        <Field
+        key={key} 
+        name={key}
+        type={key === "password" ? "password" : "text"}
+        id={key}
+        label={requiredFields[key]}
+        meta={{ error: errors[key], touched: errors[key] }}
+        required={true}
+        component={renderTextField} />
+      )
+      return result;
+    }, [])
+  }
+
   render() {
     const { handleSubmit, pristine, submitting, errors } = this.props;
+    console.log(this.state.roles)
     return (
       <form onSubmit={handleSubmit}>
-        {
-          Object.keys(requiredFields).map((field) => {
-            return(
-              <Field
-                    key={field} 
-                    name={field}
-                    type={field === "password" ? "password" : "text"}
-                    id={field}
-                    label={requiredFields[field]}
-                    meta={{ error: errors[field], touched: errors[field] }}
-                    required={true}
-                    component={renderTextField} />
-            )
-          })
-        }
+        {this.requiredFields(errors)}
+        <Field
+          key={"role_permanent_link"} 
+          name={"role_permanent_link"}
+          label={"Rol"}
+          selected={""}
+          meta={{ error: errors["role"], touched: errors["role"] }}
+          required={true}
+          component={RoleSelectDropdown}>
+            {
+              this.state.roles.map((item, key) =>
+                <MenuItem key={key} value={item.permanent_link}>
+                  <em>{item.name}</em>
+                </MenuItem>
+              )
+            }
+          </Field>
         <Button
           type="submit"
           fullWidth
           variant="contained"
           color="primary"
-          disabled={pristine || submitting}
+          disabled={pristine || submitting || !this.state.roles.length}
         >
           Guardar
         </Button>
---
+++ src/Components/Pages/Users/NewUserForm/RoleSelectDropdown.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Select         from '@material-ui/core/Select';
+import FormControl    from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import InputLabel     from '@material-ui/core/InputLabel';
+
+const hasError = function(error, touched) {
+  return error && touched && true
+}
+
+const errorMessage = function(error) {
+  if (typeof error === "string") {
+    return error
+  } else {
+    return error[0]
+  }
+}
+
+export default class RoleSelectDropdown extends Component {
+  constructor() {
+    super()
+  }
+
+  render(){
+    const { input, required, children, label } = this.props
+    const { meta: { touched, error }, ...custom } = this.props
+    return(
+      <FormControl required margin="normal" fullWidth>
+        <InputLabel htmlFor={label}>{label}</InputLabel>
+        <Select
+          value={input.value}
+          onChange={input.onChange}
+        >
+          {children}
+        </Select>
+        {hasError(error, touched)&& <FormHelperText error>{errorMessage(error)}</FormHelperText> }
+      </FormControl>
+    )
+  }
+}
