382b1b621fdf28d4492267d21e3d90210dfc788e
TauroAlde <amed157@hotmail.com>
2019-05-31 13:20:18 -0500

    [F][NTRA-26] add sort columns first_name, last_name and email

--- src/Components/Pages/Users/Index.jsx
+++ src/Components/Pages/Users/Index.jsx
@@ -19,6 +19,7 @@ import InputBase            from '@material-ui/core/InputBase';
 import SearchIcon           from '@material-ui/icons/Search';
 import CircularProgress     from '@material-ui/core/CircularProgress';
 import UsersRows            from './UsersRows';
+import TableSortLabel       from '@material-ui/core/TableSortLabel';
 
 class Users extends Component {
   constructor() {
@@ -33,6 +34,8 @@ class Users extends Component {
       searchText: "",
       timeout: 0,
       searchLoading: false,
+      sort_field: "first_name",
+      sort_direction: "desc"
     }
   }
 
@@ -42,18 +45,18 @@ class Users extends Component {
 
   handleChangeRowsPerPage = (event) => {
     let per  = event.target.value
-    let { page, searchText } = this.state
-    this.callServer({ page, per, searchText })
+    let { page, searchText, sort_direction, sort_field } = this.state
+    this.callServer({ page, per, searchText, sort_direction, sort_field })
   };
 
   ChangePage = (event, page) => {
-    let { per, searchText } = this.state
-    this.callServer({ page, per, searchText })
+    let { per, searchText, sort_direction, sort_field } = this.state
+    this.callServer({ page, per, searchText, sort_direction, sort_field })
   };
 
-  callServer({ page, per, searchText } = this.state) {
+  callServer({ page, per, searchText, sort_direction, sort_field } = this.state) {
     page++
-    let params = { params: { per, page } }
+    let params = { params: { per, page, sort: { [sort_field] : sort_direction } } }
     searchText && (params["params"]["search"] = { "all_fields.cont": searchText })
 
     this.setState({ loading: true })
@@ -68,7 +71,9 @@ class Users extends Component {
         pages: parseInt(response.data.meta.pages),
         total_records: parseInt(response.data.meta.total_records),
         searchLoading: false,
-        loading: false
+        loading: false,
+        sort_field: sort_field || this.state.sort_field,
+        sort_direction: sort_direction || this.state.sort_direction,
       })
     })
   }
@@ -76,15 +81,15 @@ class Users extends Component {
   onChangeSearch(event) {
     this.state.timeout && clearTimeout(this.state.timeout)
     const searchText =  event.target.value
-    const { page, per } = this.state
+    const { page, per, sort_direction, sort_field } = this.state
     const callServer = this.callServer.bind(this)
     searchText ?
       this.setState({
         searchText,
         searchLoading: true,
-        timeout: setTimeout(() => { callServer({ page, per, searchText }) }, 2000)
+        timeout: setTimeout(() => { callServer({ page, per, searchText, sort_direction, sort_field }) }, 2000)
       }) :
-      callServer({ page, per, searchText })
+      callServer({ page, per, searchText, sort_direction, sort_field })
     return searchText
   }
 
@@ -102,10 +107,20 @@ class Users extends Component {
     let users = this.state.users
     let index = users.findIndex(oldUser => oldUser.id === user.id)
     let updateObj = {}
-    updateObj[index] = { $set: response.data.user } 
+    updateObj[index] = { $set: response.data.user }
     this.setState({ users: update(users, updateObj) })
   }
 
+  sortHandler(field) {
+    let { page, per, searchText, sort_direction, sort_field } = this.state
+    sort_field === field ?
+      (sort_direction === "asc" ? (sort_direction = "desc") : (sort_direction = "asc")) :
+      (sort_direction = "desc")
+    return (event) => {
+      sort_field = field
+      this.callServer({ page, per, searchText, sort_direction, sort_field })
+    } 
+  } 
   render() {
     const { classes } = this.props
     return(
@@ -138,9 +153,30 @@ class Users extends Component {
             <Table className={classes.table}>
               <TableHead>
                 <TableRow>
-                  <TableCell align="center">Nombre</TableCell>
-                  <TableCell align="center">Apellido</TableCell>
-                  <TableCell align="center">Correo</TableCell>
+                  <TableCell align="center">
+                    Nombre
+                    <TableSortLabel
+                      active={this.state.sort_field === "first_name"}
+                      direction={this.state.sort_direction}
+                      onClick={this.sortHandler("first_name")}
+                    />                   
+                  </TableCell>
+                  <TableCell align="center">
+                    Apellido
+                    <TableSortLabel
+                      active={this.state.sort_field === "last_name"}
+                      direction={this.state.sort_direction}
+                      onClick={this.sortHandler("last_name")}
+                    /> 
+                  </TableCell>
+                  <TableCell align="center">
+                    Correo
+                    <TableSortLabel
+                      active={this.state.sort_field === "email"}
+                      direction={this.state.sort_direction}
+                      onClick={this.sortHandler("email")}
+                    /> 
+                    </TableCell>
                   <TableCell align="center">Opciones</TableCell>
                 </TableRow>
               </TableHead>
