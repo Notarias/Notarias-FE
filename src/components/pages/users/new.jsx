26ee24f902d856277cf0ab026a50e58efe3f078b
TauroAlde <amed157@hotmail.com>
2020-05-04 16:15:31 -0500

    [F][NTRA-86]migrate new user to graphiQL

--- src/components/pages/users/new.jsx
+++ src/components/pages/users/new.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import UserForm             from './user_form/user_form';
 import { withStyles }       from '@material-ui/core/styles';
 import Paper                from '@material-ui/core/Paper';
 import { styles }           from './user_form/styles';
-import API                  from '../../../axios_config';
-import ErrorMessage         from '../../ui/custom_snackbar_message';
-import { GENERIC_FORM_ERROR } from '../../reducers/messages_reducer';
-import { setMessage }         from '../../interfaces/messages_interface';
-import { setBreadcrumbsList } from '../../interfaces/breadcrumbs_interface';
+import UserForm from './user_form/user_form_new';
+import Breadcrumbs             from '../../ui/breadcrumbs';
+import { gql }              from 'apollo-boost';
+import { useQuery }         from '@apollo/react-hooks';
 
 const BREADCRUMBS = [
   { name: "Inicio", path: "/" },
@@ -15,69 +13,27 @@ const BREADCRUMBS = [
   { name: "Nuevo", path: null }
 ]
 
-class NewUser extends Component {
-  constructor() {
-    super()
-    this.state = {
-      errorMessage: null,
-      errors: {},
-      loading: false,
-      requiredFields: {
-        "email": "Correo Electrónico",
-        "password": "Contraseña",
-        "password_confirmation": "Confirmar Contraseña",
-        "first_name": "Nombre",
-        "last_name": "Apellido",
-      },
-    }
-  }
-
-  componentDidMount() {
-    setBreadcrumbsList(BREADCRUMBS)
-  }
-
-  submitUser = NewUserInfo => {
-    this.setState({ loading: true })
-    API.post('users',
-      {
-        user: NewUserInfo
-      }
-    ).then((data) => {
-      setMessage({ type: "success", text: "Usuario guardado, redirigiendo..." })
-      this.setState({ loading: false })
-      setTimeout(() => { this.props.history.push('/users') }, 2000)
-    }).catch((error) => {
-      if (error.response && error.response.status === 422 ) {
-        this.setState({
-          errorMessage: GENERIC_FORM_ERROR,
-          errors: error.response.data.pointers,
-          loading: false
-        })
-      }
-    });
-  }
-
-  render(){
-    const { classes } = this.props;
-    return(
-      <div className={classes.formWrapper}>
-        <Paper className={classes.paper}>
-          {this.state.errorMessage && <ErrorMessage
-            variant="error"
-            className={classes.margin}
-            message={this.state.errorMessage}
-            actionable={false}
-          />}
-          <UserForm
-            onSubmit={this.submitUser}
-            errors={this.state.errors}
-            requiredFields={this.state.requiredFields}
-            loading={this.state.loading}
-            />
-        </Paper>
-      </div>
-    );
+const GET_ROLES = gql`
+query getRoles{
+  roles{
+    name
+    permanentLink
   }
 }
+`
 
-export default withStyles(styles)(NewUser);
+const New = (props) => {
+  const { classes } = props;
+  const { data } = useQuery(GET_ROLES)
+  return(
+      <div>
+        <Breadcrumbs breadcrumbs={BREADCRUMBS}/>
+        <Paper className={classes.paper}>
+          <UserForm classes={ classes } history={props.history} data={data} />
+        </Paper>
+      </div>
+  )
+}
+
+export default withStyles(styles)(New);
+
