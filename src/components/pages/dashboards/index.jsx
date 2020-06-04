cfa8df4efdf74584fc123f479309e7598b199ab9
TauroAlde <amed157@hotmail.com>
2020-06-04 12:46:24 -0500

    [F][NTRA-90] Layout dashboard

--- src/components/pages/dashboards/index.jsx
+++ src/components/pages/dashboards/index.jsx
@@ -1,16 +1,35 @@
-import React, { Component } from 'react';
-import { setBreadcrumbsList } from './../../interfaces/breadcrumbs_interface';
+import React                        from 'react';
+import { setBreadcrumbsList }       from './../../interfaces/breadcrumbs_interface';
+import Grid                         from '@material-ui/core/Grid';
+import Paper                        from '@material-ui/core/Paper';
+import { styles }                   from './styles';
+import { withStyles }               from '@material-ui/core/styles';
+import LastPayments                 from "./index/last_payments";
+import Breadcrumbs             from '../../ui/breadcrumbs'
 
-class DashboardsIndex extends Component {
-  componentDidMount() {
+const BREADCRUMBS = [
+  { name: "Inicio", path: "/" }
+]
+
+const DashboardsIndex = (props) => {
+  const componentDidMount = () => {
     setBreadcrumbsList([])
   }
-
-  render() {
-    return (
-      <div>Home</div>
-    );
-  }
+  const { classes } = props;
+  return (
+    <>
+      <Breadcrumbs breadcrumbs={BREADCRUMBS}/>
+      <Grid container classes={{ root: classes.gridContainer }}>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>
+            <LastPayments/>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>2</Grid>
+        <Grid item xs={12} sm={4}>3</Grid>
+      </Grid>
+    </>
+  );  
 }
 
-export default DashboardsIndex;
+export default withStyles(styles)(DashboardsIndex);
---
+++ src/components/pages/dashboards/styles.js
@@ -0,0 +1,16 @@
+
+export const styles = theme => {
+  return ({
+    gridContainer: {
+      padding: theme.spacing(2),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    paperComment: {
+      height: "100%"
+    }
+  })
+}
