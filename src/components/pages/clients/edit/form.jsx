c42e2e551db97ca95569b57769e9062c725a0c12
TauroAlde <amed157@hotmail.com>
2020-11-03 20:49:19 -0500

    [F][NTRA-89] Add client attributes to client edit page

--- src/components/pages/clients/edit/form.jsx
+++ src/components/pages/clients/edit/form.jsx
@@ -1,21 +1,23 @@
-import React, { Component }       from 'react';
-import { Mutation }               from '@apollo/react-components';
-import PermIdentityIcon           from '@material-ui/icons/PermIdentity';
-import FormHelperText             from '@material-ui/core/FormHelperText';
-import PhoneRoundedIcon           from '@material-ui/icons/PhoneRounded';
-import BusinessIcon               from '@material-ui/icons/Business';
-import AssignmentIndIcon          from '@material-ui/icons/AssignmentInd';
-import EmojiTransportationIcon    from '@material-ui/icons/EmojiTransportation';
-import PersonIcon                 from '@material-ui/icons/Person';
-import MailOutlineIcon            from '@material-ui/icons/MailOutline';
-import CircularProgress           from '@material-ui/core/CircularProgress';
-import Button                     from '@material-ui/core/Button';
-import Grid                       from '@material-ui/core/Grid';
-import TextField                  from '@material-ui/core/TextField';
-import withStyles                 from '@material-ui/core/styles/withStyles';
-import { styles }                 from './../styles';
-import { GLOBAL_MESSAGE }         from '../../../../resolvers/queries';
-import { UPDATE_CLIENT_MUTATION } from '../clients_queries_and_mutations/queries';
+import React, { Component }                             from 'react';
+import { Mutation }                                     from '@apollo/react-components';
+import PermIdentityIcon                                 from '@material-ui/icons/PermIdentity';
+import FormHelperText                                   from '@material-ui/core/FormHelperText';
+import PhoneRoundedIcon                                 from '@material-ui/icons/PhoneRounded';
+import BusinessIcon                                     from '@material-ui/icons/Business';
+import AssignmentIndIcon                                from '@material-ui/icons/AssignmentInd';
+import EmojiTransportationIcon                          from '@material-ui/icons/EmojiTransportation';
+import PersonIcon                                       from '@material-ui/icons/Person';
+import MailOutlineIcon                                  from '@material-ui/icons/MailOutline';
+import CircularProgress                                 from '@material-ui/core/CircularProgress';
+import Button                                           from '@material-ui/core/Button';
+import Grid                                             from '@material-ui/core/Grid';
+import TextField                                        from '@material-ui/core/TextField';
+import withStyles                                       from '@material-ui/core/styles/withStyles';
+import { styles }                                       from './../styles';
+import { GLOBAL_MESSAGE }                               from '../../../../resolvers/queries';
+import { UPDATE_CLIENT_MUTATION, GET_CLIENT_ATTRIBUTE } from '../clients_queries_and_mutations/queries';
+import Paper                                            from '@material-ui/core/Paper';
+import { Query }                                        from '@apollo/react-components';
 
 class EditClientForm extends Component {
 
@@ -75,152 +77,194 @@ class EditClientForm extends Component {
   render() {
     const { classes } = this.props
     return (
-      <form className={ classes.newClientForm } onSubmit={(e) => e.preventDefault()}>
-        <Grid item container >
-          <Grid item xs={1}>
-            <PersonIcon classes={{root: classes.paddingInformationClient}}/>
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.firstName}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="Nombre"
-              error={this.state.errors.firstName}
-              name="firstName"/>
-            <FormHelperText error>{this.state.errors.first_name}</FormHelperText>
-          </Grid>
-        </Grid>
-        <Grid item container >
-          <Grid item xs={1}>
-            <PermIdentityIcon classes={{root: classes.genericInputIcon}}/>
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.lastName}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="Apellido"
-              error={this.state.errors.lastName}
-              name="lastName"/>
-            <FormHelperText error>{this.state.errors.last_name}</FormHelperText>
-          </Grid>
-        </Grid>
-        <Grid item container >
-          <Grid item xs={1}>
-            <EmojiTransportationIcon classes={{root: classes.genericInputIcon}}/>
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.business}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="Empresa"
-              error={this.state.errors.business}
-              name="business"/>
-            <FormHelperText error>{this.state.errors.business}</FormHelperText>
-          </Grid>
-        </Grid>
-        <Grid item container >
-          <Grid item xs={1}>
-            <AssignmentIndIcon classes={{ root: classes.genericInputIcon }}/>
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.category}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="Categoría"
-              erros={this.state.errors.category}
-              name="category"/>
-            <FormHelperText error>{this.state.errors.category}</FormHelperText>
-          </Grid>
-        </Grid>
-        <Grid item container >
-          <Grid item xs={1}>
-            <BusinessIcon classes={{root: classes.genericInputIcon}}/>
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.address}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="Dirección"
-              erros={this.state.errors.address}
-              name="address"/>
-            <FormHelperText error>{this.state.errors.address}</FormHelperText>
-          </Grid>
-        </Grid>
-        <Grid item container >
-          <Grid item xs={1}>
-            <MailOutlineIcon classes={{root: classes.genericInputIcon}} />
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.email}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="usuario@correo.com"
-              erros={this.state.errors.email}
-              name="email"/>
-            <FormHelperText error>{this.state.errors.email}</FormHelperText>
-          </Grid>
-        </Grid>
-        <Grid item container >
-          <Grid item xs={1}>
-            <PhoneRoundedIcon classes={{root: classes.genericInputIcon}}/>
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.phone}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="Teléfono"
-              erros={this.state.errors.phone}
-              name="phone"/>
-            <FormHelperText error>{this.state.errors.phone}</FormHelperText>
-          </Grid>
-        </Grid>
-        <Grid item container >
-          <Grid item xs={1}>
+      <Grid container spacing={3} align="center">
+        <Grid item xs={12}>
+          <Paper className={classes.root, classes.newClientForm} variant="outlined">
+            <form onSubmit={(e) => e.preventDefault()}>
+              <Grid item container className={classes.marginInputClient} >
+                <Grid item xs={1}>
+                  <PersonIcon classes={{root: classes.genericInputIcon}}/>
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.firstName}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="Nombre"
+                    error={this.state.errors.firstName}
+                    name="firstName"/>
+                  <FormHelperText error>{this.state.errors.first_name}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item container className={classes.marginInputClient}>
+                <Grid item xs={1}>
+                  <PermIdentityIcon classes={{root: classes.genericInputIcon}}/>
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.lastName}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="Apellido"
+                    error={this.state.errors.lastName}
+                    name="lastName"/>
+                  <FormHelperText error>{this.state.errors.last_name}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item container className={classes.marginInputClient}>
+                <Grid item xs={1}>
+                  <EmojiTransportationIcon classes={{root: classes.genericInputIcon}}/>
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.business}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="Empresa"
+                    error={this.state.errors.business}
+                    name="business"/>
+                  <FormHelperText error>{this.state.errors.business}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item container className={classes.marginInputClient}>
+                <Grid item xs={1}>
+                  <AssignmentIndIcon classes={{ root: classes.genericInputIcon }}/>
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.category}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="Categoría"
+                    erros={this.state.errors.category}
+                    name="category"/>
+                  <FormHelperText error>{this.state.errors.category}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item container className={classes.marginInputClient}>
+                <Grid item xs={1}>
+                  <BusinessIcon classes={{root: classes.genericInputIcon}}/>
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.address}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="Dirección"
+                    erros={this.state.errors.address}
+                    name="address"/>
+                  <FormHelperText error>{this.state.errors.address}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item container className={classes.marginInputClient}>
+                <Grid item xs={1}>
+                  <MailOutlineIcon classes={{root: classes.genericInputIcon}} />
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.email}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="usuario@correo.com"
+                    erros={this.state.errors.email}
+                    name="email"
+                    />
+                  <FormHelperText error>{this.state.errors.email}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item container className={classes.marginInputClient}>
+                <Grid item xs={1}>
+                  <PhoneRoundedIcon classes={{root: classes.genericInputIcon}}/>
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.phone}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="Teléfono"
+                    erros={this.state.errors.phone}
+                    name="phone"/>
+                  <FormHelperText error>{this.state.errors.phone}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item container className={classes.marginInputClient}>
+                <Grid item xs={1}>
 
-          </Grid>
-          <Grid item xs={11}>
-            <TextField
-              value={this.state.rfc}
-              classes={{ root: classes.formTextFields }}
-              onChange={this.handleChange.bind(this)}
-              label="RFC"
-              erros={this.state.errors.rfc}
-              name="rfc"/>
-            <FormHelperText error>{this.state.errors.rfc}</FormHelperText>
-          </Grid>
+                </Grid>
+                <Grid item xs={11}>
+                  <TextField
+                    value={this.state.rfc}
+                    classes={{ root: classes.formTextFields }}
+                    onChange={this.handleChange.bind(this)}
+                    label="RFC"
+                    erros={this.state.errors.rfc}
+                    name="rfc"/>
+                  <FormHelperText error>{this.state.errors.rfc}</FormHelperText>
+                </Grid>
+              </Grid>
+              <Grid item align="right">
+                <Mutation
+                  mutation={UPDATE_CLIENT_MUTATION}
+                  variables={{ ...this.state }}
+                  onCompleted={this.onCompleteCreate.bind(this)}>
+                  {
+                    (mutation, { loading, error, data }) => {
+                      return(
+                        <Button
+                        disabled={this.state.pristine || loading}
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        classes={{ root: classes.submitFormButton }}
+                        onClick={ this.submitForm.bind(this, mutation) }>
+                          Guardar Cambios
+                          { loading && <CircularProgress className={classes.buttonProgress} size={24} /> }
+                        </Button>
+                      )
+                    }
+                  }
+                </Mutation>
+              </Grid>
+            </form>
+          </Paper>
         </Grid>
-        <Grid item align="right">
-          <Mutation
-            mutation={UPDATE_CLIENT_MUTATION}
-            variables={{ ...this.state }}
-            onCompleted={this.onCompleteCreate.bind(this)}>
-            {
-              (mutation, { loading, error, data }) => {
-                return(
-                  <Button
-                    disabled={this.state.pristine || loading}
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    classes={{ root: classes.submitFormButton }}
-                    onClick={ this.submitForm.bind(this, mutation) }>
-                    Guardar Cambios
-                    { loading && <CircularProgress className={classes.buttonProgress} size={24} /> }
-                  </Button>
-                )
-              }
-            }
-          </Mutation>
+        <Grid item xs={12}>
+          <Paper className={classes.root, classes.newClientForm} variant="outlined">
+            <form onSubmit={(e) => e.preventDefault()}>
+              <Query query={GET_CLIENT_ATTRIBUTE}>
+                {
+                  ({ loading, error, data, }) => {
+                    if (loading) return "Loading...";
+                    if (error) return `Error! ${error.message}`;
+                    return (
+                      <div>
+                        {
+                          data.clientAttributes.map(atribute => (
+                            atribute.active &&
+                              <Grid item container>
+                                <Grid item xs={1}>
+                                  <PersonIcon classes={{root: classes.genericInputIcon}}/>
+                                </Grid>
+                                <Grid item xs={11}>
+                                  <TextField
+                                    classes={{ root: classes.formTextFields }}
+                                    label={atribute.name}
+                                    type={atribute.type}
+                                  />
+                                </Grid>
+                              </Grid>
+                          ))
+                        }
+                      </div>
+                    )
+                  }
+                }
+              </Query>
+            </form>
+          </Paper>
         </Grid>
-      </form>
+      </Grid>
     )
   }
 }
