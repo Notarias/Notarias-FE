5825c75f30bb4bddeb0a4bf0f2454e7728ac9839
sergio arteaga <sergio_arteaga@hotmail.com>
2021-06-22 10:29:47 -0500

    [F][NTRA-159] changing procedure and budget template step in new budget

--- src/components/pages/budgets/new/budget_selector.jsx
+++ src/components/pages/budgets/new/budget_selector.jsx
@@ -9,21 +9,21 @@ import ListItemText                                   from '@material-ui/core/Li
 import Grid                                           from '@material-ui/core/Grid';
 import Divider                                        from '@material-ui/core/Divider';
 import { useQuery }                                   from '@apollo/react-hooks';
+import { useLazyQuery } from '@apollo/react-hooks';
 import { BUDGETING_TEMPLATE_BY_PROCEDURE_ID }         from '../queries_and_mutations/queries'
 
 
 const BudgetSelector = (props) => {
   const {classes, procedureId, setbudgetInfo } = props
-  const [checked, setChecked] = React.useState([]);
   const [searchList, setSearchList] = React.useState([])
   const [initialList, setInitialList] = React.useState([])
   const [fuzzySearcher, setFuzzySearcher] = React.useState(new Fuse(initialList, { keys: ['name'] }))
-  const [initialized, setInitialized] = React.useState()
   const [selectedIndex, setSelectedIndex] = React.useState();
 
   const { loading, data, refetch } = useQuery(
-    BUDGETING_TEMPLATE_BY_PROCEDURE_ID, { variables: {"proceduresTemplateId": procedureId}, fetchPolicy: "no-cache" }
+    BUDGETING_TEMPLATE_BY_PROCEDURE_ID, { variables: {"proceduresTemplateId": procedureId} }
   );
+
 
   function changeSearch(event) {
     let result = fuzzySearcher.search(event.target.value)
@@ -35,63 +35,57 @@ const BudgetSelector = (props) => {
     }
   }
 
-  const handleToggle = (obj) => () => {
-    setbudgetInfo(obj)
-    const checkedIds = checked.map((item) => item.id)
-    const currentIndex = checkedIds.indexOf(obj.id);
-    const newChecked = [...checked];
-    if (currentIndex === -1) {
-      newChecked.push(obj);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-    setChecked(newChecked);
-  };
-
   useEffect(() => {
-    if (!initialized && data && data.budgetingTemplatesByProcedureId) {
-      setInitialList(data.budgetingTemplatesByProcedureId)
-      setFuzzySearcher(new Fuse(data.budgetingTemplatesByProcedureId, { keys: ['name'] }))
-      setSearchList(data.budgetingTemplatesByProcedureId)
-      setInitialized(true) 
-    }
-  }, [data, checked])
+    if (procedureId)
+    {setInitialList(data.budgetingTemplatesByProcedureId)
+    setFuzzySearcher(new Fuse(data.budgetingTemplatesByProcedureId, { keys: ['name'] }))
+    setSearchList(data.budgetingTemplatesByProcedureId)}
 
-  const checkedIds = checked.map((item) =>  item.id )
+  }, [data, procedureId])
+
 
   return (
     <Grid container item direction="column" alignItems="center" justify="center">
-    <div>
-      <TextField 
-        onChange={ changeSearch }
-        id="outlined-basic"
-        label="Buscar"
-        variant="outlined"
-        className={ classes.textFieldSearchInTable }
-      />
-    </div>
+      <div>
+        <TextField 
+          onChange={ changeSearch }
+          id="outlined-basic"
+          label="Buscar"
+          variant="outlined"
+          className={ classes.textFieldSearchInTable }
+          fullWidth
+        />
+      </div>
     <div className={ classes.selectableListItem }>
-      
       <List 
-          component="nav" 
-          aria-label="contacts" 
-          disablePadding={true}
-        >
+        component="nav" 
+        aria-label="contacts" 
+        disablePadding={true}
+      >
         { 
           searchList.map(
-          (item) => {
+            (item, index) => {
               let obj = item.item || item
+              const handleMenuItemClick = (event) => {
+                setSelectedIndex(obj.id);
+                setbudgetInfo(obj)
+              };
               return(
               <React.Fragment key={obj.id + "fragment"}>
-                  <ListItem key={obj.id} role={undefined} dense button onClick={handleToggle(obj)}>
-                    <ListItemText 
-                      id={obj.id} 
-                      primary={` ${ obj.name }`}
-                      checked={checkedIds.indexOf(obj.id) !== -1}
-                      tabIndex={-1}
-                    />
-                  </ListItem>
-                  <Divider/>
+                <ListItem 
+                  key={obj.id} 
+                  role={undefined} 
+                  dense 
+                  button 
+                  onClick={ handleMenuItemClick }
+                  selected={obj.id === selectedIndex}
+                >
+                  <ListItemText 
+                    id={obj.id} 
+                    primary={obj.name}
+                  />
+                </ListItem>
+                <Divider/>
               </React.Fragment>
               )
             }
--- src/components/pages/budgets/new/procedures_search.jsx
+++ src/components/pages/budgets/new/procedures_search.jsx
@@ -6,15 +6,13 @@ import Divider                                        from '@material-ui/core/Di
 import { TextField }                                  from '@material-ui/core';
 import List                                           from '@material-ui/core/List';
 import ListItem                                       from '@material-ui/core/ListItem';
-import ListItemIcon                                   from '@material-ui/core/ListItemIcon';
 import ListItemText                                   from '@material-ui/core/ListItemText';
-import Checkbox                                       from '@material-ui/core/Checkbox';
 import { useQuery }                                   from '@apollo/react-hooks';
 import { GET_PROCEDURES_TEMPLATES_QUICK_LIST  }       from '../queries_and_mutations/queries';
 import Grid                                           from '@material-ui/core/Grid';
 
 
-const renderSearchList = (searchList, checked, classes, handleToggle, setProcedureInfo) => {
+const renderSearchList = (searchList, checked, classes, setProcedureInfo, selectedIndex, setSelectedIndex) => {
   const checkedIds = checked.map((item) =>  item.id )
     return(
       <List 
@@ -24,16 +22,25 @@ const renderSearchList = (searchList, checked, classes, handleToggle, setProcedu
       >
       { 
         searchList.map(
-        (item) => {
+        (item, index) => {
             let obj = item.item || item
+            const handleMenuItemClick = (event) => {
+              setSelectedIndex(obj.id);
+              setProcedureInfo(obj);
+            };
             return(
             <React.Fragment key={obj.id + "fragment"}>
-                <ListItem key={obj.id} role={undefined} dense button onClick={handleToggle(obj)}>
+                <ListItem 
+                  key={obj.id} 
+                  role={undefined}
+                  dense
+                  button
+                  onClick={ handleMenuItemClick }
+                  selected={obj.id === selectedIndex}
+                >
                   <ListItemText 
                     id={obj.id} 
-                    primary={` ${ obj.name }`}
-                    checked={checkedIds.indexOf(obj.id) !== -1}
-                    tabIndex={-1}
+                    primary={ obj.name + "/-" + obj.budgetingTemplatesIds }
                   />
                 </ListItem>
                 <Divider/>
@@ -55,7 +62,7 @@ const ProceduresSearch = (props) => {
   const [initialList, setInitialList] = React.useState([])
   const [fuzzySearcher, setFuzzySearcher] = React.useState(new Fuse(initialList, { keys: ['name'] }))
   const [initialized, setInitialized] = React.useState()
-  const [selectedIndex, setSelectedIndex] = React.useState();
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   const { loading, data, refetch } = useQuery(
     GET_PROCEDURES_TEMPLATES_QUICK_LIST ,
@@ -63,7 +70,6 @@ const ProceduresSearch = (props) => {
 
   function changeSearch(event) {
     let result = fuzzySearcher.search(event.target.value)
-    
     if (event.target.value.length === 0){
       setSearchList(initialList)
     } else {
@@ -71,21 +77,7 @@ const ProceduresSearch = (props) => {
     }
   }
 
-  const handleToggle = (obj) => () => {
-    setProcedureInfo(obj)
-    const checkedIds = checked.map((item) => item.id)
-    const currentIndex = checkedIds.indexOf(obj.id);
-    const newChecked = [...checked];
-    if (currentIndex === -1) {
-      newChecked.push(obj);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-    setChecked(newChecked);
-  };
-
   useEffect(() => {
-    // setToLinkSelectedOption(checked)
     if (!initialized && data && data.proceduresTemplatesQuickList) {
       setInitialList(data.proceduresTemplatesQuickList)
       setFuzzySearcher(new Fuse(data.proceduresTemplatesQuickList, { keys: ['name'] }))
@@ -103,11 +95,12 @@ const ProceduresSearch = (props) => {
           label="Buscar"
           variant="outlined"
           className={ classes.textFieldSearchInTable }
+          fullWidth
         />
       </div>
       <div className={ classes.selectableListItem }>
         {
-          renderSearchList(searchList, checked, classes, handleToggle)
+          renderSearchList(searchList, checked, classes, setProcedureInfo, selectedIndex, setSelectedIndex)
         }
       </div>
     </Grid>
