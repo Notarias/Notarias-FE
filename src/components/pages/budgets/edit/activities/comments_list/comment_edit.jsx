01d59aff9b125c3af185305da0ff9451e1266b70
Rolando Gonzalez <roga.zero@gmail.com>
2022-01-21 17:28:30 -0500

    Fix unify comments view on budgets and procedures (#137)

--- src/components/pages/budgets/edit/activities/comments_list/comment_edit.jsx
+++ src/components/pages/budgets/edit/activities/comments_list/comment_edit.jsx
@@ -1,30 +1,34 @@
-import React                       from 'react'
-import { withStyles }               from '@material-ui/core/styles';
-import { styles }                   from '../../../styles';
-import Avatar                       from '@material-ui/core/Avatar';
-import Typography                   from '@material-ui/core/Typography';
-import Grid                         from '@material-ui/core/Grid';
-import Button                       from '@material-ui/core/Button';
-import TextField                    from '@material-ui/core/TextField';
-import Link                         from '@material-ui/core/Link';
-import Dialog                       from '@material-ui/core/Dialog';
-import DialogActions                from '@material-ui/core/DialogActions';
-import DialogTitle                  from '@material-ui/core/DialogTitle';
+import React, { useState }          from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import { styles } from '../../../styles';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Chip from '@material-ui/core/Chip';
+import SaveIcon from '@material-ui/icons/Save';
+import ClearIcon from '@material-ui/icons/Clear';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import { useMutation }              from '@apollo/client';
 import { UPDATE_COMMENT }           from '../../../queries_and_mutations/queries';
 import { GET_COMMENTABLE_COMMENTS } from '../../../queries_and_mutations/queries';
 import { DESTROY_COMMENT }          from  '../../../queries_and_mutations/queries';
 import { GET_BUDGETS_AUDITLOG }     from '../../../queries_and_mutations/queries';
 
-
 const CommentEdit = (props) => {
-  const { classes, comment, budget } = props
+  const { comment, budget } = props
 
-  const [commentValue, setCommentValue] = React.useState(comment.body)
-  const [commentShowed, setCommentShowed] = React.useState(true)
-  const [open, setOpen] = React.useState(false)
-  const [pristine, setPristine] = React.useState(true)
-  const [error, setError] = React.useState(false)
+  const [commentValue, setCommentValue] = useState(comment.body);
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [editStatus, setEditStatus] = useState(false);
+  const [error, setError] = useState(false);
 
   const inputsList = ["body"]
 
@@ -33,22 +37,18 @@ const CommentEdit = (props) => {
     UPDATE_COMMENT,
     {
       onError(apolloError) {
-        setErrors(apolloError)
-        setPristine(true)
+        setErrors(apolloError);
       },
       onCompleted(cacheData) {
-        setCommentShowed(true)
-        setPristine(true)
-        setError(false)
+        setEditStatus(false);
+        setError(false);
       },
       refetchQueries: [
         {
-          query: GET_COMMENTABLE_COMMENTS,
-          variables: {"commentableType": "Budget" , commentableId: budget.id }
+          query: GET_COMMENTABLE_COMMENTS, variables: {"commentableType": "Budget" , commentableId: budget.id }
         },
         {
-          query: GET_BUDGETS_AUDITLOG,  
-            variables: { "budgetId": budget.id }
+          query: GET_BUDGETS_AUDITLOG, variables: { "budgetId": budget.id }
         }
       ],
       awaitRefetchQueries: true
@@ -68,40 +68,46 @@ const CommentEdit = (props) => {
     setError(errorsList)
   }
 
-  const updateComment = (event) => {
-    !pristine && (
-      updateCommentMutation({
-        variables:{
-          "id": comment.id ,
-          "body": commentValue
-        }
-      })
-    )
-  }
-
   const [destroyCommentMutation, {loading: destroyCommentloading}] =
   useMutation(
     DESTROY_COMMENT,
     {
-      onError(apolloError) {
-        // setErrors(apolloError)
-        // setPristine(true)
-      },
       onCompleted(cacheData) {
       },
       refetchQueries: [
         {
-          query: GET_COMMENTABLE_COMMENTS,
-          variables: {"commentableType": "Budget" , commentableId: budget.id }
+          query: GET_COMMENTABLE_COMMENTS, variables: {"commentableType": "Budget" , commentableId: budget.id }
         },
         {
-          query: GET_BUDGETS_AUDITLOG,  
-            variables: {"budgetId": budget.id}
+          query: GET_BUDGETS_AUDITLOG, variables: {"budgetId": budget.id}
         }
       ],
       awaitRefetchQueries: true
     }
   )
+
+  const getCurrentDate = (comment, separator='/') => {
+    let newDate = new Date(Date.parse(comment.createdAt))
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+    let hours = newDate.getHours();
+    let minutes = newDate.getMinutes();
+  
+    return (
+      `${date}${separator}${month < 10 ? `0${month}` : `${month}`}${separator}${year} -
+        ${hours}:${minutes < 10 ? `0${minutes}` : `${minutes}`}`
+    )
+  }
+
+  const updateComment = (event) => {
+    updateCommentMutation({
+      variables:{
+        "id": comment.id ,
+        "body": commentValue
+      }
+    })
+  }
 
   const destroyComment = (event) => {
       destroyCommentMutation({
@@ -111,123 +117,139 @@ const CommentEdit = (props) => {
       })
   }
 
-  const changingInputComment = () => {
-    setCommentShowed(!commentShowed)
-    setCommentValue(comment.body)
-    setPristine(true)
-  }
-
-  const editingComment = (event) => {
+  const onChangeCommentInput = (event) => {
     setCommentValue(event.target.value)
-    setPristine(false)
   }
 
-  const handleClickOpen = () => {
-    setOpen(true);
+  const editComment = () => {
+    setCommentValue(comment.body);
+    setEditStatus(true);
+  }
+
+  const cancelEdit = () => {
+    setCommentValue(comment.body);
+    setEditStatus(false);
+  }
+
+  const openDestroyConfirm = () => {
+    setConfirmDelete(true);
   };
 
-  const handleClose = () => {
-    setOpen(false);
+  const closeDestroyConfirm = () => {
+    setConfirmDelete(false);
   };
-
-  const renderCommentEditingInput = (body) => {
-    return (
-        <Grid container item direction="column" >
-          <Grid item className={classes.commentEditingInputGrid}>
-            <TextField
-              value={commentValue}
-              onChange={editingComment}
-              autoFocus
-              size="small"
-              fullWidth
-              variant="outlined"
-              multiline
-              error={ !!error["body"] && true }
-              helperText={error["body"] || " "}
-              errorskey={ "body" }
-            />
-          </Grid>
-          <Grid container item justifyContent="flex-start" >
-            <Link
-              component="button"
-              size="small"
-              variant="body2"
-              onClick={changingInputComment}
-              className={classes.buttonTextComments}
-              color="textPrimary"
-            >
-              Cancelar
-            </Link>
-            <Link
-              component="button"
-              size="small"
-              variant="body2"
-              onClick={updateComment}
-              disabled={pristine || updateCommentLoading}
-              className={ pristine ? classes.buttonTextCommentsDisabled : classes.buttonTextComments }
-            >
-              Aceptar
-            </Link>
-          </Grid>
-        </Grid>
-    )
-  }
-
-  const renderTextComment = (body) => {
-    return(
-      <Grid container item>
-        <Grid container item className={classes.commentEditingInputGrid}>
-          <Typography variant="caption" align="left">
-            {comment.body}
-          </Typography>
-        </Grid>
-        <Grid container item>
-          <Link
-            onClick={changingInputComment}
-            className={classes.buttonTextComments}
-          >
-            Editar
-          </Link>
-          <Link
-            className={classes.buttonTextComments}
-            onClick={handleClickOpen}
-          >
-            Eliminar
-          </Link>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>
-              Se eliminará este comentario
-            </DialogTitle>
-            <DialogActions>
-              <Button onClick={handleClose}>
-                Cancelar
-              </Button>
-              <Button onClick={destroyComment} disabled={destroyCommentloading}>
-                Aceptar
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Grid>
-      </Grid>
-    )
-  }
 
   return(
-    <React.Fragment key={comment.id + "fragment-Comment"}>
-      <Grid container className={classes.fragmentComments}>
-        <Grid container item xs={2}  justifyContent="center" alignItems="flex-start">
+    <>
+      <Grid container direction="row" spacing={2}>
+        <Grid container item xs={1}  justifyContent="center" alignItems="flex-start">
           <Avatar src={ comment.user.avatarThumbUrl} size="small"/>
         </Grid>
-        <Grid container direction="column" item xs={10} justifyContent="flex-start" alignItems="flex-start">
-          <Grid container item>
-          <Typography variant="subtitle2">
-            {comment.user.firstName}  {comment.user.lastName}
-          </Typography>
+
+        <Grid container item xs={9} justifyContent="flex-start" alignItems="flex-start">
+          <Grid container item direction="row" spacing={2} justifyContent="flex-start">
+            <Grid container item xs={4} justifyContent="flex-start">
+              <Grid item>
+                <Typography variant="subtitle2">
+                  {comment.user.firstName}  {comment.user.lastName}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container item xs={8} justifyContent="flex-start">
+              <Grid item>
+                <Typography color="primary" variant="body2" align="left">
+                    {`Creado: ${getCurrentDate(comment)}`}
+                </Typography>
+              </Grid>
+            </Grid>
           </Grid>
-          {commentShowed ? renderTextComment() : renderCommentEditingInput()}
+          <Grid container item xs={12} justifyContent="flex-start">
+            { editStatus ?
+              <>
+                <Grid container item xs={12} justifyContent="flex-start">
+                  <TextField 
+                    id={comment.id}
+                    value={ commentValue } 
+                    variant="outlined" 
+                    size="small"
+                    onChange={ onChangeCommentInput }
+                    multiline
+                    fullWidth
+                  />
+                </Grid>
+                <Grid container item direction="row" xs={12} justifyContent="flex-start">
+                  <Grid item>
+                    <IconButton onClick={cancelEdit} color="secondary">
+                      <ClearIcon fontSize="small" />
+                    </IconButton>
+                  </Grid>
+                  <Grid item>
+                    <IconButton onClick={updateComment} color="primary">
+                      <SaveIcon fontSize="small" />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+              </>
+            :
+              <>
+                <Grid container item xs={12} justifyContent="flex-start">
+                  <Grid item>
+                    <Typography variant="body2" align="justify">
+                      {comment.body}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container item direction="row" xs={12} justifyContent="flex-start">
+                  <Grid item>
+                    <Button color="primary" onClick={editComment}>
+                      Editar
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button color="secondary" onClick={openDestroyConfirm}>
+                      Eliminar
+                    </Button>
+                  </Grid>
+              </Grid>
+            </>
+            }
+          </Grid>
+        </Grid>
+        <Grid container item xs={2} justifyContent="flex-end">
+          <Grid item>
+            {(comment.createdAt === comment.updatedAt) ? 
+              <></>
+              :
+              <Chip 
+                label="Editado"
+                variant="outlined"
+                color="primary"
+                style={{marginRight: '5px'}}
+              />
+            }
+          </Grid>
         </Grid>
       </Grid>
-    </React.Fragment>
+      <Divider style={{margin: 5}}/>
+      <Dialog open={confirmDelete} onClose={closeDestroyConfirm}>
+        <DialogTitle>
+          Mensaje de Confirmacion
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            ¿Seguro que desea eliminar este comentario?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={destroyComment}>
+            Confirmar
+          </Button>
+          <Button onClick={closeDestroyConfirm}>
+            Cancelar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
