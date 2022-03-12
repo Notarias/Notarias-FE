16eac7cc428ba9885a2c1dfc120ff61a54453cfc
MaverickRolex <roga.zero@gmail.com>
2022-03-12 01:21:32 -0500

    Fix review changes libraries indent and add breadcrumbs

--- src/components/pages/appointments/index.jsx
+++ src/components/pages/appointments/index.jsx
@@ -1,19 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { styles } from './styles';
-import Dialog from '@material-ui/core/Dialog';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import NewAppointmentDialog from './new/new_appointment_dialog';
-import EventList from './eventList/index';
-import { useQuery } from '@apollo/client';
-import { GET_APPOINTMENTS } from './queries_and_mutations/queries';
-import Box from '@material-ui/core/Box';
-import LoadingAppointments from './loading_appointments';
+import React, { useState, useEffect }       from 'react';
+import Breadcrumbs                          from '../../ui/breadcrumbs'
+import Box                                  from '@material-ui/core/Box';
+import Dialog                               from '@material-ui/core/Dialog';
+import Paper                                from '@material-ui/core/Paper';
+import Grid                                 from '@material-ui/core/Grid';
+import Typography                           from '@material-ui/core/Typography';
+import Button                               from '@material-ui/core/Button';
+import Calendar                             from 'react-calendar'
+import                                      'react-calendar/dist/Calendar.css';
+import NewAppointmentDialog                 from './new/new_appointment_dialog';
+import EventList                            from './eventList/index';
+import LoadingAppointments                  from './loading_appointments';
+import { withStyles }                       from '@material-ui/core/styles';
+import { styles }                           from './styles';
+import { useQuery }                         from '@apollo/client';
+import { GET_APPOINTMENTS }                 from './queries_and_mutations/queries';
+
+const BREADCRUMBS = [
+  { name: "Inicio", path: "/" },
+  { name: "Calendario", path: null }
+]
 
 const AppointmentsIndex = (props) => {
   const { classes } = props
@@ -25,7 +31,6 @@ const AppointmentsIndex = (props) => {
   const [page]              = useState(1);
   const [per]               = useState(50);
   const [date, setDate]     = useState(new Date());
-  const [appointmentsList, setAppointmentsList] = useState([]);
   const [newDialog, setNewDialog] = useState(false);
 
   let variables = {
@@ -59,58 +64,59 @@ const AppointmentsIndex = (props) => {
 
   return(
     <>
-    { 
-      loading || !data  ?
-        <LoadingAppointments/>
-      :
-        <>
-          <Grid container spacing={3} style={ { paddingTop: "30px", paddingBottom: "30px" } }>
-            <Grid item xs={4}></Grid>
-            <Grid item xs={8} container justifyContent="flex-start">
-              <Box color="primary.main">
-                <Typography variant="h4" component="h2" >{(date.toLocaleDateString('es-ES', { month: 'long', day: 'numeric', year: 'numeric' })).toUpperCase()}</Typography>
-              </Box>
-            </Grid>
-          </Grid>
-          <Grid container spacing={3} >
-            <Grid item xs={4}>
-              <Paper className={classes.marginLeftCalendarPaper} >
-                <Typography variant="h4" component="h2">Calendario</Typography>
-                <Grid container justifyContent="center" >
-                  <Calendar
-                    onChange={selectDay}
-                    value={date}
-                  />
+      <Breadcrumbs breadcrumbs={ BREADCRUMBS }/>
+        { 
+          loading || !data  ?
+            <LoadingAppointments/>
+          :
+            <>
+              <Grid container spacing={3} style={ { paddingTop: "30px", paddingBottom: "30px" } }>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={8} container justifyContent="flex-start">
+                  <Box color="primary.main">
+                    <Typography variant="h4" component="h2" >{(date.toLocaleDateString('es-ES', { month: 'long', day: 'numeric', year: 'numeric' })).toUpperCase()}</Typography>
+                  </Box>
                 </Grid>
-                <Grid className={classes.calendarNew}>
-                  <Grid >
-                    <Button variant="contained" color="primary" onClick={openNewDialog}>
-                      Nuevo Evento
-                    </Button>
+              </Grid>
+              <Grid container spacing={3} >
+                <Grid item xs={4}>
+                  <Paper className={classes.marginLeftCalendarPaper} >
+                    <Typography variant="h4" component="h2">Calendario</Typography>
+                    <Grid container justifyContent="center" >
+                      <Calendar
+                        onChange={selectDay}
+                        value={date}
+                      />
+                    </Grid>
+                    <Grid className={classes.calendarNew}>
+                      <Grid >
+                        <Button variant="contained" color="primary" onClick={openNewDialog}>
+                          Nuevo Evento
+                        </Button>
+                      </Grid>
+                    </Grid>
+                    <Dialog onClose={closeNewDialog} aria-labelledby="simple-dialog-title" open={newDialog}>
+                      <NewAppointmentDialog closeNewDialog={closeNewDialog} getAppointmensVariables={variables}/>
+                    </Dialog>
+                  </Paper>
+                </Grid>
+
+                <Grid item xs={8}>
+                  <Grid>
+                    {data.appointments.length > 0 ?
+                      data.appointments.map(appointment  => {
+                        return(<EventList key={`dashboard-appointment-${appointment.id}`} appointment={appointment} getAppointmensVariables={variables} />)
+                      })
+                    :
+                      <Paper style={{ padding: "30px" }}>
+                        <Typography variant='h4'>Sin Eventos</Typography>
+                      </Paper>
+                    }
                   </Grid>
                 </Grid>
-                <Dialog onClose={closeNewDialog} aria-labelledby="simple-dialog-title" open={newDialog}>
-                  <NewAppointmentDialog closeNewDialog={closeNewDialog} getAppointmensVariables={variables}/>
-                </Dialog>
-              </Paper>
-            </Grid>
-
-            <Grid item xs={8}>
-              <Grid>
-                {data.appointments.length > 0 ?
-                  data.appointments.map(appointment  => {
-                    return(<EventList key={`dashboard-appointment-${appointment.id}`} appointment={appointment} getAppointmensVariables={variables} />)
-                  })
-                :
-                  <Paper style={{ padding: "30px" }}>
-                    <Typography variant='h4'>Sin Eventos</Typography>
-                  </Paper>
-                }
               </Grid>
-            </Grid>
-          </Grid>
-        </>
-    }
+            </>
+        }
     </>
   )
 }
