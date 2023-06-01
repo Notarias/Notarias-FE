03351bfd9a421d2b40b8a4c1560c51feb08f24d6
MaverickRolex <roga.zero@gmail.com>
2023-06-01 14:48:35 -0500

    fix apolo config file

---
+++ src/apollo.js
@@ -0,0 +1,81 @@
+import { ApolloClient }         from '@apollo/client';
+import { InMemoryCache }        from '@apollo/client';
+import { onError }              from 'apollo-link-error';
+import { ApolloLink, from }     from '@apollo/client/core';
+import { BatchHttpLink }        from 'apollo-link-batch-http';
+
+import history                  from './history'
+import { resolvers, typeDefs }  from './resolvers/resolvers';
+import { createUploadLink }     from 'apollo-upload-client';
+import { createConsumer }       from "@rails/actioncable"
+import ActionCableLink          from 'graphql-ruby-client/dist/subscriptions/ActionCableLink'
+
+export const cache = new InMemoryCache();
+
+export const BASE_URI = 'notarias-rails.herokuapp.com'
+//export const BASE_URI = 'localhost:3000'
+
+// Change protocol https to http for localhost
+const URI = `https://${BASE_URI}/graphql`;
+
+const uploadLink = new createUploadLink({ uri: URI });
+// Change protocol wss to ws for localhost
+
+const cableTokenUriAssignation = function() {
+  return(`https://${BASE_URI}/cable?token=${localStorage.getItem('jwtToken')}`)
+}
+
+const cable = createConsumer(cableTokenUriAssignation)
+
+const batchLinkHttp = new BatchHttpLink({ uri: URI })
+
+let httpLink = ApolloLink.split(
+  operation => operation.getContext().hasUpload,
+  uploadLink,
+  batchLinkHttp
+)
+
+let httpLinkConnection = ApolloLink.split(
+  ({ query: { definitions } }) => {
+    return definitions.some(
+      ({ kind, operation }) => {
+        return(kind === 'OperationDefinition' && operation === 'subscription')
+      }
+    )
+  },
+  new ActionCableLink({cable}),
+  httpLink
+)
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext(({ headers = {} }) => {
+    const result = {
+      headers: {
+        ...headers,
+        Authorization: localStorage.getItem('jwtToken') || null,
+      }
+    }
+    return(result)
+  });
+
+  return forward(operation);
+})
+
+const logoutLink = onError(({ networkError, forward, operation }) => {
+  if (networkError && networkError.statusCode === 401) {
+    localStorage.clear();
+    history.push(`/sign_in`)
+  }
+})
+
+const apolloClient = new ApolloClient(
+  {
+    cache,
+    link: from([authMiddleware, logoutLink, httpLink]),
+    typeDefs,
+    resolvers
+  }
+);
+
+export default apolloClient
